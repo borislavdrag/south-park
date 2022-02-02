f2e7b100886575d59a9db938e7b83a25ee4637c7
Borislav Dragandzhikov <boris@MacBook-Pro-von-Bobby.local>
2022-02-02 01:52:16 +0100

    added naive bayes model option

--- pipelines.py
+++ pipelines.py
@@ -7,12 +7,12 @@ from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer 
 
 
-def get_pipe(vec_method, model_type):
+def get_pipe(model_type='xgboost', vec_method='tfidf'):
 
     vector_pipe = {
         'tfidf': TfidfVectorizer(max_features=500),
 
-      #  'word2vec':
+      #  'word2vec':  # DLC
     }
 
     model_pipe = {
@@ -29,17 +29,12 @@ def get_pipe(vec_method, model_type):
                                   n_jobs=8,
                                   verbosity=1))
         ]),
-      #  'bayes': Pipeline([
-      #  ('vectorizer', vector_pipe[vec_method]),
-      #  ('clf', nb.MultinomialNB(objective='multi:softprob',
-      #                           eval_metric='merror',
-      #                           max_depth=3,
-      #                           n_estimators=40,
-      #                           learning_rate=0.1,
-      #                           use_label_encoder=False,
-      #                           n_jobs=8,
-      #                           verbosity=1))
-      #  ])
+        'bayes': Pipeline([
+        ('columns', KeepColumns(cols=['Zweck'])),
+        ('preprocess', CleanData(covariate='Zweck')), 
+        ('vectorizer', vector_pipe[vec_method]),
+        ('clf', nb.MultinomialNB())
+        ])
     }
 
     return model_pipe[model_type]
